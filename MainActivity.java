c9b672e4774f1b86b745e7258d314b8550233e6d
Harshita Bhardwaj <57848574+silverarrow975@users.noreply.github.com>
2021-09-28 21:33:28 +0000

    Choices now change color when selected

--- MainActivity.java
+++ MainActivity.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private String[] qList;
+    private int qNum; // index of the question we are on in the qList
     private ArrayList<ArrayList<Double>> ansChoices;
 
     //progress bar stuff
@@ -27,7 +28,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private boolean isSubmit;
     private TextView q;
     private Button[] choices;
-    private boolean[] choiceClicked;
+    private ArrayList<Boolean> choiceClicked = new ArrayList<Boolean>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +40,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         submit = findViewById(R.id.submit);
         isSubmit = true;
         q = findViewById(R.id.question);
+        qNum = 0;
 
         // initialize the choice buttons
         choices = new Button[4];
-        choiceClicked = new boolean[4]; // shows which button is selected
-        for(boolean test : choiceClicked){
-            System.out.println(test);
-        }
+        choiceClicked = new ArrayList<Boolean>(); // shows which button is selected
+
 
         for(int i = 0; i < choices.length; i++) {
             // cant just use this string in the find view by ide function because it doesn't accept a string
@@ -58,8 +58,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             choices[i].setBackgroundColor(Color.parseColor("#d4d4d4"));
             //what to do when the button is pressed
             choices[i].setOnClickListener(this);
-            choiceClicked[i] = false;
+            choiceClicked.add(false); // initialize the choice clicked array
         }
+
 
         // initialize questions and answers list
         qList = new String[10];
@@ -74,15 +75,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             qList[i] = ques.toString();
             ansChoices.add(ques.answerChoices()); // add the ans choices for each question
         }
+        
+        // setting up what that page looks like when it opens
+        q.setText(qList[qNum]);
 
 
     }
 
-    /*
-  https://www.youtube.com/watch?v=vFziVSSIX6Q&list=PLxzIyQWl5sMNvkLJ3yARdx7n7xSlRviI5&index=8
-    */
     @Override
     public void onClick(View v) {
+        /* Start Button */
+
+
+        /* selecting an ans choice */
+
+        String choiceID = v.getResources().getResourceEntryName(v.getId()); // get the id of the clicked button
+        int choiceIndex = Integer.parseInt(choiceID.substring(choiceID.length() - 1)); // the index of the button in the list
+
+        // if an answer was already picked
+        if(choiceClicked.contains(true)){
+            // reset the list
+            for(int f = 0; f < choiceClicked.size(); f++)
+            {
+                choiceClicked.set(f, false);
+                choices[f].setBackgroundColor(Color.parseColor("#d4d4d4"));
+            }
+        }
+        choiceClicked.set(choiceIndex, true); // update the choice clicked array
+        choices[choiceIndex].setBackgroundColor(Color.parseColor("#9e9e9e")); // make the selected choice button darker
+
+
+        /* Onclick listener for the submit button */
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
